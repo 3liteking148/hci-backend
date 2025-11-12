fae4af64ce8e21386a8d81b2ddf3a5ececd4d07b
Harvey Chua <harvs89.2@gmail.com>
2025-11-12 18:24:10 +0800

    increase upload limit

--- src/server.ts
+++ src/server.ts
@@ -24,7 +24,7 @@ const app = express();
 // **** Middleware **** //
 
 // Basic middleware
-app.use(express.json());
+app.use(express.json({limit: '100mb' }));
 app.use(express.urlencoded({extended: true}));
 
 // hack
